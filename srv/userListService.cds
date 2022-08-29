3c043d6328532987f9fa96a12f0e4d086cdb0df3
PPG-BrianChan <cchan@ppg.com>
2022-08-29 06:27:35 +0000

    Cleanup db model

--- srv/userListService.cds
+++ srv/userListService.cds
@@ -1,18 +1,7 @@
-using userListService as uls from '../db/data-model';
-
 service uls_listService @(
-    // requires : 'system-user',
+    requires : 'system-user',
     path     : 'API'
 ) {
-    @cds.persistence.skip
-    entity users  as projection on uls.users;
-
-    @cds.persistence.skip
-    entity groups as projection on uls.groups;
-
-    //to be renamed
-    function getUsersWithParams(requrl : String, reqparams : String) returns String;
-
     //action
     action getUsers(requrl: String, reqparams : String) returns String;
 }
--- srv/userListService.js
+++ srv/userListService.js
@@ -1,50 +1,10 @@
 cds.env.odata.protectMetadata = false
 const cdsapi = require('@sapmentors/cds-scp-api');
-const usersurl = `/v1.0/users/`;
-const groupmembersurl = `/v1.0/groups/<groupID>/members/`;
-const groupsurl = `/v1.0/groups/`;
 const destination = "Microsoft_Graph_User_List_API";
 const error_code_blankKey = 'Bad Request';
 const error_msg_blankKey = 'Key value requrl cannot be blank';
 
 module.exports = (srv) => {
-    const { users, groups } = srv.entities;
-
-    srv.on('READ', 'users', (req) => {
-    })
-
-    srv.on('getUsersWithParams', async (req, res) => {
-        var url = '';
-        
-        if (req.data.requrl == undefined || req.data.requrl == null || req.data.requrl == '') {
-            req.error({"code":error_code_blankKey,"message":error_msg_blankKey})
-            return;
-        }else{
-            url = req.data.requrl;
-        }
-        
-        if (req.data.reqparams !== undefined && req.data.reqparams !== null && req.data.reqparams !== '') {
-            url = url.concat('?', req.data.reqparams);
-        }
-        const service = await cdsapi.connect.to(destination);
-
-        try {
-
-            const results = await service.run({
-                url: url,
-                method: "get",
-                headers: {
-                    'ConsistencyLevel': 'Eventual'
-                },
-            })
-            return results;
-        }
-        catch (error) {
-            // console.log(error.response.data.error)
-            req.error({"code":error.response.data.error.code,"message":error.response.data.error.message})
-        }
-    })
-
 
     srv.on('getUsers', async(req, res) => {
         var url = '';
@@ -75,8 +35,5 @@ module.exports = (srv) => {
         catch (error) {
             req.error({"code":error.response.data.error.code,"message":error.response.data.error.message})
         }
-    // var res = req._.req.res;
-    // res.send("Test")
-        // return("Test")
     })
 }
